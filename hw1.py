e5dbf10548f96606c6a6ce0bbb5086472a454d5b
Viacheslav Diachenko@home <viacheslav.diachenko@gmail.com>
2023-03-26 19:11:26 +0300

    Додано приклад використання параметра greeting

--- hw1.py
+++ hw1.py
@@ -1,6 +1,7 @@
-def greet(name, greeting="Привіт"):
+	def greet(name, greeting="Привіт"):
     return f"{greeting}, {name}!"
 
 if __name__ == "__main__":
     name = "Світ"
     print(greet(name))
+    print(greet(name, "Вітаю"))
